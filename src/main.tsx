d1ab04eb4e3baa6269e5bd4afbb4164aa08cf1cb
Ismael Britzke <isma_steffens@hotmail.com>
2026-04-10 08:37:13 -0300

    implementacao

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
 import { ThemeContextProvider } from "./contexts/ThemeContext";
+import { registerSW } from 'virtual:pwa-register'
 
+registerSW({ immediate: true })
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
     <ThemeContextProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'],
+      manifest: {
+        name: 'Seu App',
+        short_name: 'App',
+        description: 'Meu sistema em versão app',
+        theme_color: '#0f172a',
+        background_color: '#0f172a',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
